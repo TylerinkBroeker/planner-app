452a84953a62d90514574d5ed5ea81b09c2b054e
Tyler Broeker <tylerinkbroeker@gmail.com>
2022-02-18 16:38:53 -0800

    Home and Navbar
    Added pages and components folder with Home.js file and Navbar.js with placeholder tabs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
+
+
   return (
     <div className="App">
+      <Navbar />
       <div className='content'>
-        <h1>Planner!</h1>
+        <Home />
       </div>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+
+    return (
+        <nav className='navbar'>
+            <div className='links'>
+                <a href="/">Home</a>
+                <a href="/day">Day View</a>
+                <a href="/week">Week View</a>
+                <a href="/month">Month View</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
